15dc975c3fc0f6c3d58236519179eb1a430d2111
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-10-03 20:02:13 +0200

    create ForWhom component

---
+++ src/components/ForWhom/ForWhom.vue
@@ -0,0 +1,97 @@
+<template>
+  <section class="content__section">
+    <h2 class="forwhom__h2">for whom?</h2>
+    <div class="forwhom">
+      <div class="forwhom__image-box-1 content__dont-display-on-mobile">
+        <img
+          src="/images/potr_pots_2.jpg"
+          alt=" "
+          class="forwhom__image-1 content__dont-display-on-mobile"
+        />
+      </div>
+
+      <div class="forwhom__article-1">
+        <div
+          class="forwhom__keyword content__dont-display-on-mobile forwhom__keyword--1"
+        >
+          <div class="forwhom__star"></div>
+          Robust
+        </div>
+
+        <div class="forwhom__title forwhom__title--1">
+          <span class="forwhom__inline forwhom__inline--1">
+            For those who value durable things
+          </span>
+        </div>
+
+        <div class="forwhom__text forwhom__text--1">
+          Don’t worry if you happen to knock your POTR on the floor, unlike a
+          traditional ceramic pot it will take the fall in its stride.
+        </div>
+      </div>
+
+      <div class="forwhom__image-box-2 content__dont-display-on-mobile">
+        <img
+          src="/images/potr_pots_4.jpg"
+          alt=" "
+          class="forwhom__image-2 content__dont-display-on-mobile"
+        />
+      </div>
+
+      <div class="forwhom__article-2">
+        <div
+          class="forwhom__keyword forwhom__keyword--2 content__dont-display-on-mobile"
+        >
+          <div class="forwhom__star"></div>
+          Beautiful
+        </div>
+
+        <div class="forwhom__title forwhom__title--2">
+          <span class="forwhom__inline forwhom__inline--2">
+            For those who love modern design
+          </span>
+        </div>
+
+        <div class="forwhom__text forwhom__text--2">
+          We strived to achieve something that would look beautiful in your home
+          while being incredibly functional and intuitive to use.
+        </div>
+      </div>
+
+      <div class="forwhom__image-box-3 content__dont-display-on-mobile">
+        <img
+          src="/images/potr_pots_5.jpg"
+          alt=" "
+          class="forwhom__image-3 content__dont-display-on-mobile"
+        />
+      </div>
+
+      <div class="forwhom__article-3">
+        <div
+          class="forwhom__keyword forwhom__keyword--3 content__dont-display-on-mobile"
+        >
+          <div class="forwhom__star"></div>
+          Saves time
+        </div>
+
+        <div class="forwhom__title forwhom__title--3">
+          <span class="forwhom__inline forwhom__inline--3">
+            For those who don't have enough time to regular water indoor plants
+          </span>
+        </div>
+
+        <div class="forwhom__text forwhom__text--3">
+          Water is drawn up through the cotton by the plant using tried and
+          tested wicking technology. It's particularly useful if you aren’t able
+          to water them yourself on a regular basis!
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "ForWhom",
+};
+</script>
