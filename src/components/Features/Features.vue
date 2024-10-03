ad184bff5bcfd2b64873077db5ee054df1dda3e3
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-10-03 20:14:59 +0200

    create Features component

---
+++ src/components/Features/Features.vue
@@ -0,0 +1,65 @@
+<template>
+  <section class="content__section">
+    <h2 class="features__h2">
+      POTR Pots
+      <span class="features__h2 features__h2--stroke"> Features</span>
+    </h2>
+
+    <div class="features">
+      <article class="features__card">
+        <img
+          src="/images/potr_pots_6.jpg"
+          alt="flat pack postage"
+          class="features__image"
+        />
+
+        <div class="features__title">Flat pack postage</div>
+
+        <div class="features__text">
+          The pots will be shipped flat packed so they can be slipped through
+          your letter box
+        </div>
+      </article>
+
+      <article class="features__card">
+        <img
+          src="/images/potr_pots_7.jpg"
+          alt="Easy assembly"
+          class="features__image"
+        />
+
+        <div class="features__title">Easy assembly</div>
+
+        <div class="features__text">
+          POTR Pots are super easy (and super satisfying) to assemble using the
+          quick draw cotton cord
+        </div>
+      </article>
+
+      <article class="features__card">
+        <img
+          src="/images/potr_pots_8.jpg"
+          alt="Self-watering"
+          class="features__image"
+        />
+
+        <div class="features__title">Self-watering</div>
+
+        <div class="features__text">
+          Simply add water into the pot — if your plant is thirsty it will
+          automatically start drinking the water through the cotton cord. Magic!
+        </div>
+      </article>
+    </div>
+  </section>
+
+  <div class="content__team-bcg"></div>
+
+  <div class="content__marker" id="about"></div>
+</template>
+
+<script>
+export default {
+  name: "Features",
+};
+</script>
