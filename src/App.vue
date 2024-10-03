29bb4f1a25fcc0f41925320eb1366d7c693ee4f0
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-10-03 20:15:34 +0200

    include Features, ForWhom and Team components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup>
 import Header from "./components/Header/Header.vue";
 import ForWhom from "./components/ForWhom/ForWhom.vue";
+import Features from "./components/Features/Features.vue";
+import Team from "./components/Team/Team.vue";
 </script>
 
 <template>
   <Header />
-
   <main>
     <ForWhom />
+    <Features />
+    <Team />
   </main>
 </template>
