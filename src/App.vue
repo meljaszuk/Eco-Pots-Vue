8928196c09dafac5b8f49b61a4ff66cbcf4cb1f9
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-10-03 20:02:26 +0200

    include forWhom in app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <script setup>
 import Header from "./components/Header/Header.vue";
+import ForWhom from "./components/ForWhom/ForWhom.vue";
 </script>
 
 <template>
   <Header />
+
+  <main>
+    <ForWhom />
+  </main>
 </template>
