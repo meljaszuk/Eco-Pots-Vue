50a8c62df6d19bc3b59878f1e21ad9d3ab1a02b4
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-10-03 17:07:55 +0200

    import main scss

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./main.scss";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
