3b681b42cced0d31967608dc890d20982bf821fa
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-10-03 20:50:50 +0200

    add ScreenAlert component

---
+++ src/components/ScreenAlert/ScreenAlert.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="screen-alert">
+    <img src="/icons/logo.png" alt=" " class="screen-alert__logo" />
+    <div class="screen-alert__p">
+      This project has been tailored for optimal viewing on
+      <strong> screens with a width of 320 pixels or wider. </strong>
+      <br />
+      Kindly consider using a different device for an enhanced experience.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ScreenAlert",
+};
+</script>
