c808ffe1d797deccb3553341624ba94db7139f3f
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-10-03 20:41:03 +0200

    include new components in app

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@ import Header from "./components/Header/Header.vue";
 import ForWhom from "./components/ForWhom/ForWhom.vue";
 import Features from "./components/Features/Features.vue";
 import Team from "./components/Team/Team.vue";
+import Materials from "./components/Materials/Materials.vue";
+import Product from "./components/Product/Product.vue";
+import Benefits from "./components/Benefits/Benefits.vue";
+import Contact from "./components/Contact/Contact.vue";
 </script>
 
 <template>
@@ -11,5 +15,10 @@ import Team from "./components/Team/Team.vue";
     <ForWhom />
     <Features />
     <Team />
+    <Materials />
+    <Product />
+    <Benefits />
+    <Contact />
   </main>
+  <Footer />
 </template>
