bb045c8b590989bbbc474b000d6567e58f831551
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-10-03 20:39:45 +0200

    create Product component

---
+++ src/components/Product/Product.vue
@@ -0,0 +1,50 @@
+<template>
+  <section class="content__section">
+    <div class="size">
+      <h2 class="size__h2">Which size POTR for my house plant ?</h2>
+
+      <div class="size__grid">
+        <article class="size__intro">
+          Pre potted plants come in fairly standard sized pots, and can be
+          classified into one of two categories
+        </article>
+
+        <div class="size__img-box">
+          <img
+            src="/images/potr_pots_sizes.jpg"
+            alt="potr pots sizes"
+            class="size__image"
+          />
+        </div>
+
+        <article class="size__options">
+          <strong class="size__strong"> LARGE </strong>
+
+          <p class="size__p">
+            Large POTR Pot = 14cm diameter (1.5 litre) house plant pot or less
+          </p>
+
+          <strong class="size__strong"> SMALL </strong>
+
+          <p class="size__p">
+            Small POTR Pot = 10cm diameter (0.49 litre) house plant pot or less
+          </p>
+        </article>
+
+        <article class="size__star">
+          POTR Pots have a little bit of give around the top rim so can fit
+          slightly larger sizes than the recommended sizes stated above if
+          necessary.
+        </article>
+      </div>
+    </div>
+  </section>
+
+  <div class="content__marker"></div>
+</template>
+
+<script>
+export default {
+  name: "Product",
+};
+</script>
